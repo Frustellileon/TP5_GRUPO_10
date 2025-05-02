3b94cf1c2687dd2cf2b13450c9543eedf31a36af
AlejoTomasAlbor <alejotomasalbor@gmail.com>
2025-05-02 18:31:46 -0300

    Inicio de clase SQL - Puntos de la clase, tarea 1

---
+++ TP5_GRUPO_10/ClaseSQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TP5_GRUPO_10
+{
+    public class ClaseSQL
+    {
+        ///Atributos de la clase
+        private string cadenaConexion = "";
+        private SqlConnection conexion;
+        private SqlCommand sqlCommand;
+        private SqlDataReader sqlDataReader;
+        private SqlDataAdapter sqlDataAdapter;
+        private DataSet dataSet;
+        private bool estado = false; //Serviria como control para saber si la conexion esta abierta o cerrada
+
+        ///----------------------------------------------------------  Funciones de la clase  --------------------------------------------------------------------------
+
+        //Constructor de la clase
+        public ClaseSQL(string cadena)
+        {
+            //Remplaza la url de la coneccion
+            cadenaConexion = cadena;
+
+            //Seteo por las dudas el estado inicial en desconectado
+            estado = false;
+        }
+    }
+}
