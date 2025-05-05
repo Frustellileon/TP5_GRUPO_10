9749fc08bc625476623a20562716980afb8d0811
Flor2345 <flor2016.disney@gmail.com>
2025-05-04 23:20:55 -0300

    Cambios

--- TP5_GRUPO_10/ListadoSucursales.aspx.cs
+++ TP5_GRUPO_10/ListadoSucursales.aspx.cs
@@ -11,35 +11,29 @@ namespace TP5_GRUPO_10
 {
     public partial class ListadoSucursales : System.Web.UI.Page
     { 
-<<<<<<< HEAD
+//<<<<<<< HEAD
         private const string consultaSQL = "SELECT Id_Sucursal, NombreSucursal AS Nombre, DescripcionSucursal AS Descripcion, DescripcionProvincia AS Provincia, DireccionSucursal AS Direccion FROM Sucursal INNER JOIN Provincia ON Id_ProvinciaSucursal = Id_Provincia";
         private readonly ClaseSQL claseSQL = new ClaseSQL();
-        
-=======
-        private string consultaSQL = "SELECT Id_Sucursal, NombreSucursal AS Nombre, DescripcionSucursal AS Descripcion, DescripcionProvincia AS Provincia, DireccionSucursal AS Direccion FROM Sucursal INNER JOIN Provincia ON Id_ProvinciaSucursal = Id_Provincia";
-<<<<<<< HEAD
-        ClaseSQL claseSQL = new ClaseSQL();
-=======
-        ClaseSQL cc = new ClaseSQL();
->>>>>>> Validaciones Ejercicio 2 (Es numerico e ID existente) - Se incorpora en la ClaseSQL el metodo para validar existencia de ID .
->>>>>>> Validaciones Ejercicio 2 (Es numerico e ID existente) - Se incorpora en la ClaseSQL el metodo para validar existencia de ID .
+
         protected void Page_Load(object sender, EventArgs e)
         {
             System.Web.UI.ValidationSettings.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
 
             if (!IsPostBack)
             {
-<<<<<<< HEAD
-=======
+//<<<<<<< HEAD
+//=======
                 // Abro la conexion
                 claseSQL.AbrirConexion();
                 
->>>>>>> Validaciones Ejercicio 2 (Es numerico e ID existente) - Se incorpora en la ClaseSQL el metodo para validar existencia de ID .
                 // Ejecuto la consulta enviandola al metodo como parametro
                 gvSucursales.DataSource = claseSQL.CargarGridView(consultaSQL).Tables[0];
                 
                 // Muestro la tabla con DataBind
                 gvSucursales.DataBind();
+
+                // Cierro la conexion
+                claseSQL.CerrarConexion();
             }
         }
 
@@ -58,7 +52,7 @@ namespace TP5_GRUPO_10
             gvSucursales.DataBind();
         }
 
-        protected System.Void cv_IsInexistente_ServerValidate(System.Object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
+        protected void cv_IsInexistente_ServerValidate(System.Object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
         {
             if (rev_VNumerico.IsValid)
             {
